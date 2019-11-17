a567b714108de950491383836313bff6f3aa8077
Wandmalfarbe <Wandmalfarbe@users.noreply.github.com>
2019-11-17 17:03:54 +0100

    no message

--- R/eisvogel.r
+++ R/eisvogel.r
@@ -1,3 +1,13 @@
+find_resource <- function(template, file = 'template.tex') {
+  res <- system.file(
+    "rmarkdown", "templates", template, "resources", file, package = "eisvogel"
+  )
+  if (res == "") stop(
+    "Couldn't find template file ", template, "/resources/", file, call. = FALSE
+  )
+  res
+}
+
 
 # Helper function to create a custom format derived from pdf_document that
 # includes a custom LaTeX template
